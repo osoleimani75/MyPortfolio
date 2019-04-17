1db662f572b3442ca4ce1cd75a6c8866e0736dcf
osoleimani75 <osoleimani75@gmail.com>
2019-04-16 21:46:48 -0700

    new portfolio

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var exphbs  = require('express-handlebars');
+const path = require("path");
+var app = express();
+
+app.use(express.static("public"));
+
+const hbs = exphbs.create({
+    defaultLayout: 'masterPage',
+    layoutsDir   : path.join(__dirname, '/views'),
+    helpers      : path.join(__dirname, '/Public/helpers'),
+    partialsDir  : path.join(__dirname, '/views/Partials')
+});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+ 
+app.get('/', function (req, res) {
+    res.render('layouts/home');
+});
+
+app.get('/experience', function (req, res) {
+    res.render('layouts/experience');
+});
+app.get('/contact', function (req, res) {
+    res.render('layouts/contact');
+});
+
+app.listen(3001);
